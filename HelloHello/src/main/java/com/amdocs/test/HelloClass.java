60fe24d8b0593b7680027317d290a2859dae7620
vipulj <vipuljha47@gmail.com>
2016-07-26 18:14:05 +0530

    first Commit

---
+++ HelloHello/src/main/java/com/amdocs/test/HelloClass.java
@@ -0,0 +1,20 @@
+package com.amdocs.test;
+
+
+public class HelloClass {
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	    System.out.println(new HelloClass().hello());
+	    System.out.println("Hey");
+		}
+		public String hello(){
+			return "Hello World!!!";
+		}
+
+	
+
+
+}
---
+++ HelloHello/src/test/java/com/amdocs/test/HelloClassTest.java
@@ -0,0 +1,17 @@
+package com.amdocs.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class HelloClassTest extends TestCase {
+
+		String message = "Hello World!!!";
+		HelloClass h = new HelloClass();
+		
+		@Test
+		public void testHello() {
+			assertEquals(h.hello(),message);
+		}
+
+	}
