1fbbbf3c8b035f7a190645f6111bc3cde17c9e35
vipulj <vipuljha47@gmail.com>
2016-07-29 20:18:17 +0530

    HI vipul commit

--- HelloHello/src/main/java/com/amdocs/test/HelloClass.java
+++ HelloHello/src/main/java/com/amdocs/test/HelloClass.java
@@ -8,7 +8,7 @@ public class HelloClass {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 	    System.out.println(new HelloClass().hello());
-	    System.out.println("Hey");
+	    System.out.println("Hey Vipul");
 		}
 		public String hello(){
 			return "Hello World!!";
